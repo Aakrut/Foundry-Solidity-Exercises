d61121d014d8cf30188cd7c20dcd1fe99314b031
aakrut <aakrutdabhi@gmail.com>
2023-03-29 10:45:25 +0530

    add sol ex-10

---
+++ DeployContract/src/DeployContract.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+// ✅
+contract Deployer {
+    /*
+        This exercise assumes you know how to deploy a contract.
+        1. Create an empty contract.
+        2. Deploy the contract and return the address in `deployContract` function.
+    */
+
+    function deployContract() public view returns (address) {
+        return address(this);
+
+    }
+}
