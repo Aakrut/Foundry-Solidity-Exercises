6f6287bca07f76c5194c1155e91387fcde7d66f3
aakrut <aakrutdabhi@gmail.com>
2023-03-30 12:11:08 +0530

    add sol ex-21

---
+++ Fibonacci/src/Fibonacci.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+// ✅
+contract Fibonacci {
+    /*
+        This exercise assumes you understand what Fibonacci sequence is.
+        1. Function `fibonacci` takes a uint256 as argument and returns nth fibonacci number.
+        
+        Fibonacci sequence are 0,1,1,2,3,5,8,13,....
+        
+        calling fibonacci(6) would return 8, because the 6th Fibonacci number is 8.
+    */
+
+    function fibonacci(uint256 _position) public view returns (uint256) {
+        if (_position == 0) {
+            return 0;
+        } else if (_position == 1) {
+            return 1;
+        } else {
+            uint256 ans = fibonacci(_position - 1) + fibonacci(_position - 2);
+            return ans;
+        }
+    }
+}
