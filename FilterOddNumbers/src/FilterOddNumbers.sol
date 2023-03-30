cfdd8104d3db3f94581cd394f2c52ca4e802a98f
aakrut <aakrutdabhi@gmail.com>
2023-03-30 12:11:28 +0530

    add sol ex-22

---
+++ FilterOddNumbers/src/FilterOddNumbers.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+// ✅
+contract FilterOddNumbers {
+    /*
+        This exercise assumes you understand how to manipulate Array.
+        1. Function `filterOdd` takes an array of uint256 as argument. 
+        2. Filter and return an array with the odd numbers removed.
+        Note: this is tricky because you cannot allocate a dynamic array in memory, 
+              you need to count the even numbers then declare an array of that size.
+    */
+
+    function filterOdd(uint256[] memory _arr)
+        public
+        view
+        returns (uint256[] memory)
+    {
+      uint256 evenCount = 0;
+      for(uint256 i = 0; i < _arr.length;i++) {
+        if(_arr[i] % 2 == 0) {
+            evenCount ++;
+        }
+      }
+
+      uint256[] memory newArr = new uint256[](evenCount);
+      uint256 j = 0;
+      for(uint256 i = 0; i < _arr.length;i++) {
+        if(_arr[i] % 2 == 0) {
+          newArr[j] = _arr[i];
+          j++;
+        } 
+      }
+      return newArr;
+    }
+}
