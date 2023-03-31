b0e5175e5cd020e3478d6aa25f3ad012e3c0d23d
aakrut <aakrutdabhi@gmail.com>
2023-03-31 12:06:26 +0530

    add sol ex-30

---
+++ IsSorted/src/IsSorted.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+
+contract IsSorted {
+    /**
+     * The goal of this exercise is to return true if the members of "arr" is sorted (in ascending order) or false if its not.
+     */
+    function isSorted(uint256[] calldata arr) public pure returns (bool) {
+        // your code here
+        for(uint256 i = 1; i < arr.length; i++) {
+            if(arr[i] < arr[i-1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
