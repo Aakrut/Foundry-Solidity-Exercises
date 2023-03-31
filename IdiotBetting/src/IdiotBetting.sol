88df3aaa2436ae78d15e2dff6a716a878da5586a
aakrut <aakrutdabhi@gmail.com>
2023-03-31 12:03:58 +0530

    add sol ex-24

---
+++ IdiotBetting/src/IdiotBetting.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+// ✅
+contract IdiotBettingGame {
+    /*
+        This exercise assumes you know how block.timestamp works.
+        - Whoever deposits the most ether into a contract wins all the ether if no-one 
+          else deposits after an hour.
+        1. `bet` function allows users to deposit ether into the contract. 
+           If the deposit is higher than the previous highest deposit, the endTime is 
+           updated by current time + 1 hour, the highest deposit and winner are updated.
+        2. `claimPrize` function can only be called by the winner after the betting 
+           period has ended. It transfers the entire balance of the contract to the winner.
+    */
+    address payable public winner;
+    uint public highestDeposit;
+    uint public endTime;
+
+    function bet() public payable {
+        if (msg.value > highestDeposit) {
+            winner = payable(msg.sender);
+            highestDeposit = msg.value;
+            endTime = block.timestamp + 1 hours;
+        }
+    }
+
+    function claimPrize() public {
+        require(block.timestamp >= endTime, "Betting period has not ended yet");
+        require(msg.sender == winner, "Only winner can claim the prize");
+
+        uint balance = address(this).balance;
+        (bool success, ) = payable(winner).call{value: balance}("");
+        require(success);
+    }
+}
