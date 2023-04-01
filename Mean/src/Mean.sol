4ce2d5aea3c9e2e7789423e8be13a5337df079ca
aakrut <aakrutdabhi@gmail.com>
2023-04-01 10:22:38 +0530

    add sol ex-33

---
+++ Mean/src/Mean.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+// ✅
+contract Mean {
+    /**
+     * The goal of this exercise is to return the mean of the numbers in "arr"
+     */
+    function mean(uint256[] calldata arr) public pure returns (uint256) {
+        // your code here
+        uint256 sum = 0;
+        for(uint256 i = 0; i< arr.length;i++) {
+            sum+=arr[i];
+        }
+        uint256 answer = sum / arr.length;
+        return answer;
+    }
+}
