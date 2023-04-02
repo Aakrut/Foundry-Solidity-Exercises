cc21af96cd50d976efa2d0a0a8037b3cfc5e4374
aakrut <aakrutdabhi@gmail.com>
2023-04-02 11:34:07 +0530

    add sol ex-55

---
+++ Tupledore/src/Tupledore.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+// ✅
+contract Tupledore {
+    /* This exercise assumes you know about tuples/struct in solidity.
+        1. Create a struct named `UserInfo` with types address 
+           and uint256.
+        2. Create a variable of type UserInfo, named `userInfo`.
+        3. Create a function called `setTuple` that takes in 
+           a address and uint256 and sets the all values 
+           the `userInfo` variable you created above.
+        4. Create a function called `returnTuple`, 
+           that returns `userInfo` (as a tuple)
+    */
+   struct UserInfo {
+      address addr;
+      uint256 value;
+   }
+
+   UserInfo public userInfo;
+
+   function setTuple(address _addr,uint256 _value) public {
+      userInfo = UserInfo(_addr,_value);
+   }
+
+   function returnTuple() public view returns(UserInfo memory) {
+      return UserInfo(userInfo.addr,userInfo.value);
+   }
+}
